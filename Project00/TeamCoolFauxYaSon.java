e1e4e53d2d425c0812f58961edfe6b45355fc588
Tim <fauxt@moravian.edu>
2018-02-09 08:59:09 -0500

    TeamCoolFauxYaSon.java

---
+++ Project00/TeamCoolFauxYaSon.java
@@ -0,0 +1,26 @@
+package Project00;
+
+import java.util.*;
+import Project00.Team;
+import Project00.Member;
+import Project00.SchaperMember;
+
+
+public class TeamCool extends Team
+{
+    public TeamCool(String name)
+    {
+        super(name);
+        createTeamMembers();
+    }
+
+    public void createTeamMembers()
+    {
+        members.add(new SchaperMember("Greg Schaper"));
+        members.add(new SchaperMember("Greg Schaper"));
+        members.add(new SchaperMember("Greg Schaper"));
+	members.add(new FauxMember("Tim Faux"));
+        // create a new member class for each team member
+        // and add it to the team members list
+    }
+}
